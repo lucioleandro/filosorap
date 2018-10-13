b5ba323dfb9638c00aa98994468f515716dce472
lioh <fl.araujo95@gmail.com>
2018-10-13 18:24:21 -0300

    Criação de botão e postagem compartilhada para facebook

--- js/social.js
+++ js/social.js
@@ -1,9 +1,13 @@
-var botao = document.getElementById("botao");
+var botaoTwitter = document.getElementById("botaoTwitter");
+var botaoFacebook = document.getElementById("botaoFacebook");
 
-botao.addEventListener("click", function() {
-
+botaoTwitter.addEventListener("click", function() {
 	let alink = montaLinkTwitter();
+	window.open(alink);
+});
 
+botaoFacebook.addEventListener("click", function() {
+	let alink = montaLinkFacebook();
 	window.open(alink);
 });
 
@@ -12,6 +16,17 @@ function montaLinkTwitter() {
 	let poesia = document.getElementById("estrofe").textContent;
 	let poeta = document.getElementById("poeta").textContent;
 	let alink = "https://twitter.com/intent/tweet?text="+poesia+"%0A - "+poeta+"%0A%0A"+url;
+
+	return alink;
+}
+
+function montaLinkFacebook() {
+	//let url = window.location.href;
+	let url = "www.filosorap.com.br"
+	console.log(url);
+	let poesia = document.getElementById("estrofe").textContent;
+	let poeta = document.getElementById("poeta").textContent;
+	let alink = "https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2F"+url+"&quote="+poesia+"%0A - "+poeta+"%0A%0A";
 
 	return alink;
 }
