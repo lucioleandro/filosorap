6a0dfc0a4399eb4bfe5c6c400462b545641d9a77
Lucio Leandro <lucio.leo85@gmail.com>
2018-10-30 12:53:47 -0300

    Mudando icone do whatsapp e adicionando comportamento do botão do whatsapp no social.js

--- js/social.js
+++ js/social.js
@@ -1,5 +1,6 @@
 var botaoTwitter = document.getElementById("botaoTwitter");
 var botaoFacebook = document.getElementById("botaoFacebook");
+var botaoWhatsapp = document.getElementById("botaoWhatsapp");
 
 botaoTwitter.addEventListener("click", function() {
 	let alink = montaLink("twitter");
@@ -11,17 +12,23 @@ botaoFacebook.addEventListener("click", function() {
 	window.open(alink);
 });
 
+botaoWhatsapp.addEventListener("click", function() {
+	let alink = montaLink("whatsapp");
+	window.open(alink);
+});
+
 function montaLink(social) {
 	let url = window.location.href;
 	let poesia = document.getElementById("estrofe").textContent;
 	let poeta = document.getElementById("poeta").textContent;
 	let alink = ""
 	if(social === "facebook") {
-		url = "www.filosorap.com.br"
 		alink = "https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2F"+url+
 		"&quote="+poesia+"%0A - "+poeta+"%0A%0A";
-	} else  if(social === "twitter"){
+	} else  if(social === "twitter") {
 		alink = "https://twitter.com/intent/tweet?text="+poesia+"%0A - "+poeta+"%0A%0A"+url;
+	} else if(social === "whatsapp") {
+		alink = "https://api.whatsapp.com/send?text="+poesia+"%0A - "+poeta+"%0A%0A"+url;
 	}
 	return alink;
 }
